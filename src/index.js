e696210ed23bc846510f0b1aaf60cbf0211adef0
Forrest Li <forrest.li@kitware.com>
2018-09-04 12:07:07 -0400

    feat(RodEditor): Initial port of the rod editor

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import TestProperty from './TestProperty';
 import CellEditor from './CellEditor';
+import RodEditor from './RodEditor';
 
 function registerWidgets(Simput) {
   if (Simput.registerWidget) {
     Simput.registerWidget('test', TestProperty);
     Simput.registerWidget('CellEditor', CellEditor);
+    Simput.registerWidget('RodEditor', RodEditor);
   }
 }
 
 if (typeof window !== 'undefined' && window.Simput) {
   registerWidgets(window.Simput);
 }
-
