a0721f5712fa000b1be663aca394f74e81884d7f
Forrest Li <forrest.li@kitware.com>
2018-09-06 11:56:55 -0400

    feat(AssemblyEditor): Initial 2D editor import

---
+++ src/AssemblyEditor/script.js
@@ -0,0 +1,73 @@
+import MapEditor from '../MapEditor';
+import ViewerWidget from '../ViewerWidget';
+
+// ----------------------------------------------------------------------------
+
+export default {
+  name: 'RodEditor',
+  components: {
+    MapEditor,
+    ViewerWidget,
+  },
+  props: {
+    prop: {
+      required: true,
+    },
+    viewData: {
+      required: true,
+    },
+  },
+  data() {
+    return {
+      is2D: true,
+      help: false,
+    };
+  },
+  computed: {
+    viz() {
+      return this.prop.ui.domain;
+    },
+    viewerData() {
+      return Object.assign(
+        {
+          selected: this.viewData.id,
+        },
+        this.prop.ui.domain
+      );
+    },
+    items() {
+      return ['0'].concat(
+        Object.keys(this.viz.rods).filter(
+          (id) => this.viz.rods[id].type === this.viewData.mapInfo.type.value[0]
+        )
+      );
+    },
+  },
+  methods: {
+    convertToRGB(obj) {
+      const rgbMap = {};
+      const keys = Object.keys(obj);
+      while (keys.length) {
+        const key = keys.pop();
+        rgbMap[key] = `rgb(${obj[key].map((i) => Math.floor(i * 255))})`;
+      }
+      return rgbMap;
+    },
+
+    isVisible() {
+      return this.prop.show(this.viewData);
+    },
+    onModeChange(mode) {
+      this.is2D = mode;
+    },
+  },
+  beforeMount() {
+    const update = () => this.$nextTick(this.$forceUpdate);
+    this.unsubscribe = this.$store.state.templates.dataManager.subscribe(
+      update
+    ).unsubscribe;
+  },
+  beforeDestroy() {
+    this.unsubscribe();
+  },
+};
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import TestProperty from './TestProperty';
 import CellEditor from './CellEditor';
 import RodEditor from './RodEditor';
+import AssemblyEditor from './AssemblyEditor';
 
 function registerWidgets(Simput) {
   if (Simput.registerWidget) {
     Simput.registerWidget('test', TestProperty);
     Simput.registerWidget('CellEditor', CellEditor);
     Simput.registerWidget('RodEditor', RodEditor);
+    Simput.registerWidget('AssemblyEditor', AssemblyEditor);
   }
 }
 
