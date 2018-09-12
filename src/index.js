54bcd1d80faba93f82d59a8ab25bf3e97eabd85c
Forrest Li <forrest.li@kitware.com>
2018-09-12 14:42:03 -0400

    feat(StateLabelEditor): Port over state label editor

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import CellEditor from './CellEditor';
 import RodEditor from './RodEditor';
 import AssemblyEditor from './AssemblyEditor';
 import CoreEditor from './CoreEditor';
+import StateLabelEditor from './StateLabelEditor';
 
 function registerWidgets(Simput) {
   if (Simput.registerWidget) {
@@ -11,6 +12,7 @@ function registerWidgets(Simput) {
     Simput.registerWidget('RodEditor', RodEditor);
     Simput.registerWidget('AssemblyEditor', AssemblyEditor);
     Simput.registerWidget('CoreEditor', CoreEditor);
+    Simput.registerWidget('StateLabelEditor', StateLabelEditor);
   }
 }
 
